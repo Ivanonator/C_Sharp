4cce5546f782d7a39f48e09f2efc07d8043b72c1
Иван Климентьев <ivanklimentev@MacBook-Air-Ivan.local>
2023-10-07 22:41:44 +0300

    IfElse

---
+++ Desktop/c#/Examples/Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username == "Ванька")
+{
+    Console.Write("Здарова ");
+    Console.WriteLine(username);
+}
+else
+{
+    Console.Write("Привет ");
+    Console.WriteLine(username);
+}
