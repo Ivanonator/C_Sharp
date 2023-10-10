83cf736e41956c4259a24f571a259d86600ecf37
Иван Климентьев <ivanklimentev@MacBook-Air-Ivan.local>
2023-10-10 12:50:19 +0300

    FirstWorkIsDone

---
+++ Desktop/c#/Homeworks/Homework001_SecondNumber/Program.cs
@@ -0,0 +1,5 @@
+﻿System.Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int ostatok = number % 100;
+int SecondNumber = ostatok / 10;
+System.Console.WriteLine(SecondNumber);
