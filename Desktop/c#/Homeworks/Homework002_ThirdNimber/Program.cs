1a2b7c827657f3bc3f5b04d0cfff01d8a58df431
Иван Климентьев <ivanklimentev@MacBook-Air-Ivan.local>
2023-10-16 19:02:34 +0300

    HW is done

---
+++ Desktop/c#/Homeworks/Homework002_ThirdNimber/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number / 1000 != 0)
+    {
+       while (Number / 1000 == 0)
+       {
+        int num = Number % 10;
+       }
+    }
+else if (Number / 100 == 0)
+{
+System.Console.WriteLine("Третьей цифры нет!");
+}
+else 
+{
+    System.Console.WriteLine(Number/100);
+}
