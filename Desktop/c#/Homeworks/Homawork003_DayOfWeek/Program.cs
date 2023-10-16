011bdbd81dbd37dfecec76dbb99270e5922e4ae9
Иван Климентьев <ivanklimentev@MacBook-Air-Ivan.local>
2023-10-16 19:03:49 +0300

    HW3 is done

---
+++ Desktop/c#/Homeworks/Homawork003_DayOfWeek/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Введите номер дня недели");
+int NumDayOfWeek = Convert.ToInt32(Console.ReadLine());
+if (NumDayOfWeek == 6 || NumDayOfWeek == 7)
+{
+    System.Console.WriteLine("да");
+}
+else if (NumDayOfWeek == 1 || NumDayOfWeek == 2 || NumDayOfWeek == 3 || NumDayOfWeek == 4 || NumDayOfWeek == 5)
+{
+       System.Console.WriteLine("нет");
+}
+else
+{
+    System.Console.WriteLine("такого дня недели не существует");
+}
